b593a09932af3bda4fbf4046cdd1add3381df14c
retroradar <retroradar@gmx.de>
2021-01-25 22:13:08 +0100

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="flex flex-center">
+      Welcome to retroradar.de! This page is still under construction..
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
